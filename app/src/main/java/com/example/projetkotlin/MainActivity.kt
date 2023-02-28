ffd479aa400bb4c556726516519b3fe2ed105ca8
henri pham <henri-pham@live.fr>
2023-02-28 21:53:10 +0100

    Feature terminée il reste juste à faire els sauvegardes

--- app/src/main/java/com/example/projetkotlin/MainActivity.kt
+++ app/src/main/java/com/example/projetkotlin/MainActivity.kt
@@ -2,21 +2,47 @@ package com.example.projetkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerViewToDo : RecyclerView
+    var newArrayList : ArrayList<ToDo> = arrayListOf(
+        ToDo(false,"hello"),
+        ToDo(false,""),
+        ToDo(false,""),
+    )
+    private val adapter = ToDoAdapter(newArrayList)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerViewToDo =findViewById(R.id.toDoList)
+        recyclerViewToDo.adapter = adapter
+        recyclerViewToDo.layoutManager = LinearLayoutManager(this)
+        recyclerViewToDo.setHasFixedSize(true)
 
-        val recyclerViewItem = findViewById<RecyclerView>(R.id.toDoList)
-        recyclerViewToDo.adapter = ToDoAdapter()
+
+        val addButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        addButton.setOnClickListener {
+            val toDoAdapter = adapter
+            val newItem = ToDo(checked = false, libelle = "")
+            toDoAdapter.addItem(newItem)
+        }
+
 
 
 
     }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/projetkotlin/ToDoAdapter.kt
+++ app/src/main/java/com/example/projetkotlin/ToDoAdapter.kt
@@ -4,37 +4,57 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-    class ToDoAdapter(
-        var items : List<ToDo>,
+class ToDoAdapter(
+        val items : ArrayList<ToDo>,
     ) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() {
 
 
 
+        class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var checked :CheckBox =itemView.findViewById(R.id.checkBox)
+            var libelle : EditText = itemView.findViewById(R.id.edit_text)
+            var deleteButton : ImageButton =itemView.findViewById(R.id.deleteItem)
+
+
+        }
+
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-
             val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item,parent,false)
             return ToDoViewHolder(itemView)
         }
 
         override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+            val currentItem = items[position]
+            holder.checked.isChecked = currentItem.checked
+            holder.libelle.setText(currentItem.libelle)
+            holder.deleteButton.setOnClickListener{
+                val position = holder.adapterPosition
+                items.removeAt(position)
+                notifyDataSetChanged()
+            }
+
 
         }
 
         override fun getItemCount(): Int {
             return items.size
         }
-        class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            var checked :CheckBox
-            var libelle : TextView
 
-            init {
-                checked=itemView.findViewById(R.id.checkBox)
-                libelle=itemView.findViewById(R.id.edit_text)
-            }
-        }
+    fun addItem(newItem: ToDo) {
+        items.add(newItem)
+        notifyDataSetChanged()
+    }
+
+
+
+
     }
